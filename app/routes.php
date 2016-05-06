68b72c57f55361416c0c536413068981de645d63
James Jones <jamjon3@gmail.com>
2016-05-06 14:01:28 -0400

    Making "gradual changes" so I don't break it this time migrating from the enhanced, but unfortuately broken, code base (still good)

---
+++ app/routes.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * Configure URL routes
+ * see: http://www.slimframework.com/docs/objects/router.html
+ *
+ */
+
+// Application Home Page
+$app->get('/', 'AuthTransfer\AuthToken\Action\HomeAction:dispatch')
+    ->setName('homepage');
+
+// Display images from a webservice.  The album id piece of URL is optional.
+$app->get('/example[/{album_id}]', 'AuthTransfer\AuthToken\Action\ExampleAction:dispatch')
+    ->setName('displayPlaceholderImages');
+
+// If you need to use POST or PUT you can config the route like this.
+//$app->post('/example', 'AuthTransfer\AuthToken\Action\ExampleAction:runThisMethod')
+//    ->setName('examplePOST');
